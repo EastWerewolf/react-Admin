714a4cf479518db0c385aa822f809117984118b0
EastWerewolf <lessissmore@163.com>
2019-01-02 14:58:37 +0800

    新增axios utils的引入方式

--- src/config/componentPlus.js
+++ src/config/componentPlus.js
@@ -1,25 +1,23 @@
+import {Component} from'react'
+import {get,post,put,del} from './axios'
+import {cookie,deepCopy} from './utils'
 /**
  * 扩展React Component
  * 将公共方法挂载在构造函数里
  * 用于替代React.Component
  * 也可以使用修饰符的方式扩展挂载全局方法
  */
-import React,{Component} from'react'
-import {get,post,put,del} from './axios'
-
 class ComponentPlus extends Component{
     constructor(){
         super()
-        this.axios={get,post,put,del}
-        this.get = get;
-        this.post = post;
-        this.put = put;
-        this.del = del;
+        this.axios={get,post,put,del};
+        this.utils = {cookie,deepCopy};
     }
 }
 
 /**
  * 这个是注入方式挂载axios
+ * 用法: @axios(type) 修饰符ES7
  * @param type{string}
  * @returns {Function}
  */
@@ -34,6 +32,22 @@ function axios(type){
     }
 }
 
+/**
+ * 同样的方式注入工具类函数
+ * 用法:@utils(type)
+ * @param type
+ * @returns {Function}
+ */
+function utils(type){
+    return function (target) {
+        const utils = {cookie,deepCopy}
+        if(!type){
+            target.prototype.utils = utils
+        }else{
+            target.prototype[type] = utils[type]
+        }
+    }
+}
 /**
  * 日志输出
  * @param target 被修饰的目标Class
@@ -52,4 +66,4 @@ function log(target, name, descriptor) {
     return descriptor;
 }
 
-export  {ComponentPlus,log,axios}
+export  {ComponentPlus,log,axios,utils}
