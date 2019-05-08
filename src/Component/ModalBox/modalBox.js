cdd5d60d02298a6da45476bbb8761c62b94c4e26
lessissmore@163.com <lessissmore@163.com>
2019-05-08 17:51:59 +0800

    addMore

--- src/Component/ModalBox/modalBox.js
+++ src/Component/ModalBox/modalBox.js
@@ -5,7 +5,10 @@ import './modelBox.scss'
 /**
  * prop{
  *     title:string
+ *     okText:string
+ *     cancelText:string
  *     onOK:function
+ *     width:number
  * }
  */
 class ModalBox extends Component {
@@ -22,6 +25,31 @@ class ModalBox extends Component {
     showLoading(confirmLoading){
         this.setState({confirmLoading});
     }
+    componentDidMount() {
+        const Timer = setInterval(()=>{
+            const Modal = document.getElementsByClassName('ant-modal');
+            if(Modal[0]){
+                const width = this.props.width?this.props.width:650;
+                for(let i = 0;i < Modal.length;i++){
+                    Modal[i].style.width = width + 'px';
+                }
+                clearInterval(Timer)
+            }
+        },500);
+    }
+    componentWillReceiveProps(nextProps, nextContext) {
+        const Timer = setInterval(()=>{
+            const Modal = document.getElementsByClassName('ant-modal');
+            if(Modal[0]){
+                const width = this.props.width?this.props.width:650;
+                for(let i = 0;i < Modal.length;i++){
+                    Modal[i].style.width = width + 'px';
+                }
+                clearInterval(Timer)
+            }
+        },500);
+    }
+
     render() {
         const { visible, confirmLoading} = this.state;
         return (
@@ -31,6 +59,9 @@ class ModalBox extends Component {
                 onOk={this.props.handleOk}
                 confirmLoading={confirmLoading}
                 onCancel={()=>this.showModal(false)}
+                afterClose={this.props.afterClose}
+                okText={this.props.okText||'确定'}
+                cancelText={this.props.cancelText||'取消'}
             >
                 {this.props.children}
             </Modal>
